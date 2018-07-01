9bcaccc63e2cf36990a2aea8ac7f38229cc60274
Charles Kenney <charlesc.kenney@gmail.com>
2018-07-01 12:55:30 -0400

    update concurrency model

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
 	memeBot = newBot(conf.Discord.BotToken)
 	memeBot.AddHandler(handlePutMemeMessage)
 	memeBot.AddHandler(handleGetMemeMessage)
 	memeBot.Listen()
-	<-make(chan struct{})
+
+	log.Info("Bot has booted")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	memeBot.Session.Close()
 }
